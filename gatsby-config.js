ebaf8f362ae1ffa993b940c7f94b417078c3cb21
Tuan Nguyen <56967422+tbtuan@users.noreply.github.com>
2020-06-23 15:04:50 +0200

    unsafe eval

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const plugins = [
       mergeScriptHashes: false,
       mergeStyleHashes: false,
       directives: {
-        "script-src": `'self' 'unsafe-inline'`,
+        "script-src": `'self' 'unsafe-inline' 'unsafe-eval'`,
         "style-src": `'self' 'unsafe-inline'`,
         "style-src": `'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com`,
         "font-src": `'self' data: fonts.gstatic.com`,
