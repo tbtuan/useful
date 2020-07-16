aad8b8b4f13108d70f8412ed920e11a1eb32b132
Tuan Nguyen <56967422+tbtuan@users.noreply.github.com>
2020-07-16 22:59:31 +0200

    adjust networktimeout

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,36 +68,34 @@ const plugins = [
     resolve: "gatsby-plugin-offline",
     options: {
       workboxConfig: {
-        importWorkboxFrom: `local`,
-        cacheId: `gatsby-plugin-offline`,
-        // Don't cache-bust JS or CSS files, and anything in the static directory,
-        // since these files have unique URLs and their contents will never change
-        dontCacheBustURLsMatching: /(\.js$|\.css$|static\/)/,
         runtimeCaching: [
           {
-            // Use cacheFirst since these don't need to be revalidated (same RegExp
-            // and same reason as above)
             urlPattern: /(\.js$|\.css$|static\/)/,
             handler: `CacheFirst`,
           },
           {
-            // page-data.json files are not content hashed
-            urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
+            urlPattern: /^https?:.*\/page-data\/.*\/(page-data|app-data)\.json$/,
             handler: `NetworkFirst`,
+            options: {
+              networkTimeoutSeconds: 3,
+            },
           },
           {
-            // Add runtime caching of various other page resources
             urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
             handler: `StaleWhileRevalidate`,
           },
           {
-            // Google Fonts CSS (doesn't end in .css so we need to specify it)
             urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
             handler: `StaleWhileRevalidate`,
           },
+          {
+            urlPattern: /\/$/,
+            handler: `NetworkFirst`,
+            options: {
+              networkTimeoutSeconds: 3,
+            },
+          },
         ],
-        skipWaiting: true,
-        clientsClaim: true,
       },
     },
     // options: {
