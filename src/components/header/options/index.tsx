03953abb1f0d29acf66026fb4747f45c53dda062
Tuan Nguyen <56967422+tbtuan@users.noreply.github.com>
2021-02-28 22:43:31 +0100

    bars

--- src/components/header/options/index.tsx
+++ src/components/header/options/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { Container } from "./style";
+import React, { useState } from "react";
+import {
+  Container,
+  MobileContainer,
+  BarsIcon,
+  Menu,
+  ContainerWrapper,
+} from "./style";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const OptionsLayout = ({ children }: Props) => (
-  <Container>{children}</Container>
-);
+const OptionsLayout = ({ children }: Props) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <MobileContainer>
+        <Menu htmlFor="" onClick={() => setShow((prevShow) => !prevShow)}>
+          <BarsIcon />
+        </Menu>
+        <ContainerWrapper show={show}>{children}</ContainerWrapper>
+      </MobileContainer>
+      <Container>{children}</Container>
+    </>
+  );
+};
 
 export default OptionsLayout;
--- src/components/header/options/style.ts
+++ src/components/header/options/style.ts
@@ -1,4 +1,33 @@
 import styled from "@emotion/styled";
+import Bars from "icons/Bars.svg";
+
+interface Props {
+  show: boolean;
+}
+
+export const BarsIcon = styled(Bars)`
+  cursor: pointer;
+  width: 1.25rem;
+  height: 1.25rem;
+  fill: ${({ theme }) => theme.colors.switch};
+  filter: drop-shadow(0px 4px 5px ${({ theme }) => theme.colors.switchShadow});
+  :hover {
+    opacity: 85%;
+  }
+`;
+
+export const MobileContainer = styled("div")`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  margin-left: auto;
+  padding-right: 3rem;
+  display: none;
+
+  @media only screen and (max-width: 576px) {
+    display: block;
+  }
+`;
 
 export const Container = styled("div")`
   display: flex;
@@ -10,8 +39,40 @@ export const Container = styled("div")`
     padding-right: 3rem;
     width: 12.5rem;
   }
-
   @media only screen and (max-width: 576px) {
     display: none;
+  }
+`;
+
+export const Menu = styled("label")`
+  display: flex;
+  user-select: none;
+`;
+
+export const ContainerWrapper = styled("div")`
+  display: ${(props: Props) => (props.show ? `flex` : `none`)};
+  flex-direction: row;
+  position: absolute;
+  z-index: -2;
+  top: calc(100% + 0.5em - 3rem);
+  width: 40vw;
+  filter: drop-shadow(0px 4px 5px ${({ theme }) => theme.colors.searchShadow});
+  background: ${({ theme }) => theme.colors.search};
+  margin-left: 2rem;
+  gap: 1rem;
+  align-items: center;
+  margin-left: auto;
+
+  border-radius: 0.25rem;
+  * {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  @media only screen and (max-width: 576px) {
+    top: calc((100% + 0.5em) - 4rem);
+    width: 100%;
+    padding: 5rem 1.5rem 1rem;
+    left: 0;
   }
 `;
